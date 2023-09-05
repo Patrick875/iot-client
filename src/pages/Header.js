8044de4efd265d6712710ce85790ec234ca83803
KUBWIMANA Patrick <52809972+Patrick875@users.noreply.github.com>
2023-09-05 12:40:43 +0200

    removed logout button

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -15,12 +15,7 @@ function Header() {
 							Smart Jacket System
 						</Link>
 					</div>
-					<div className=" col-4 d-flex justify-content-end align-items-center">
-						<Link className="btn  btn-outline-light " to="/">
-							<i className="px-2 bi bi-door-open" />
-							Logout
-						</Link>
-					</div>
+					
 				</div>
 			</header>
 		</div>
